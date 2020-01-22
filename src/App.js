4b01e9c12c8ec8c7f3aae7ae0c22c83f008f8d9e
anthonyqcn <anthonyqcn@gmail.com>
2020-01-22 00:32:33 -0800

    update styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [messages, setMessages] = useState([]);
+
+	console.log(messages);
+	return (
+		<main>
+			<header>
+				<img
+					className="logo"
+					alt="logo"
+					src="https://www.freeiconspng.com/uploads/christmas-flake-geometric-hexagon-holiday-line-snow---3.png"
+				/>
+				Chatter
+			</header>
+
+			<div className="messages">
+				{messages.map((m, i) => {
+					return (
+						<div key={i} className="message-wrap">
+							<div className="message">{m}</div>
+						</div>
+					);
+				})}
+			</div>
+
+			<TextInput
+				onSend={text => {
+					setMessages([text, ...messages]);
+				}}
+			/>
+		</main>
+	);
+}
+
+function TextInput(props) {
+	var [text, setText] = useState('');
+	// normal js comment
+	return (
+		<div className="text-input-wrap">
+			<input
+				value={text}
+				className="text-input"
+				placeholder="Type your message here"
+				onChange={e => setText(e.target.value)}
+				onKeyPress={e => {
+					if (e.key === 'Enter') {
+						if (text) props.onSend(text);
+						setText('');
+					}
+				}}
+			/>
+			<button
+				onClick={() => {
+					if (text) props.onSend(text);
+					setText('');
+				}}
+				className="button"
+				disabled={!text}
+			>
+				SEND
+			</button>
+		</div>
+	);
 }
 
 export default App;

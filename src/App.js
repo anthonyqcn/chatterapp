a06898d333380bfae2ff3f9d82c854c0c1cd6750
anthonyqcn <anthonyqcn@gmail.com>
2020-01-29 10:02:39 -0800

    fix camera

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { db, useDB } from './db.js';
 import NamePicker from './namePicker.js';
 import { Browser, Route, BrowserRouter } from 'react-router-dom';
 import Camera from 'react-snap-pic';
-import TextInput from './textinput.js';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/storage';
+import { MdSend, MdCamera } from 'react-icons/md';
 
 // function takePicture(img) {
 // 	console.log(img);
@@ -31,9 +31,15 @@ function Room(props) {
 	const messages = useDB(room);
 	const [name, setName] = useState('');
 	const [showCamera, setShowCamera] = useState(false);
-	function takePicture(img) {
+	async function takePicture(img) {
 		setShowCamera(false);
-		return console.log(img);
+		const imgID = Math.random()
+			.toString(36)
+			.substring(7);
+		var storageRef = firebase.storage().ref();
+		var ref = storageRef.child(imgID + '.jpg');
+		await ref.putString(img, 'data_url');
+		db.send({ img: imgID, name, ts: new Date(), room });
 	}
 
 	return (
@@ -52,20 +58,9 @@ function Room(props) {
 			</header>
 
 			<div className="messages">
-				{messages.map((m, i) => {
-					return (
-						<div
-							key={i}
-							className="message-wrap"
-							from={m.name === name ? 'me' : 'you'}
-						>
-							<div className="message">
-								<div className="msg-name">{m.name}</div>
-								<div className="msg-text">{m.text}</div>
-							</div>
-						</div>
-					);
-				})}
+				{messages.map((m, i) => (
+					<Message key={i} m={m} name={name} />
+				))}
 			</div>
 
 			<TextInput
@@ -80,6 +75,60 @@ function Room(props) {
 				showCamera={() => setShowCamera(true)}
 			/>
 		</main>
+	);
+}
+
+const bucket =
+	'https://firebasestorage.googleapis.com/v0/b/chatter2020-7dc4e.appspot.com/o/';
+const suffix = '.jpg?alt=media';
+
+function TextInput(props) {
+	var [text, setText] = useState('');
+	return (
+		<div className="text-input-wrap">
+			<button
+				onClick={props.showCamera}
+				style={{ position: 'absolute', left: 2, top: 10 }}
+			>
+				<MdCamera style={{ height: 15, width: 15 }} />
+			</button>
+			<input
+				value={text}
+				className="text-input"
+				placeholder="Type your message here"
+				onChange={e => setText(e.target.value)}
+				onKeyPress={e => {
+					if (e.key === 'Enter') {
+						if (text) props.onSend(text);
+						setText('');
+					}
+				}}
+			/>
+			<button
+				onClick={() => {
+					if (text) props.onSend(text);
+					setText('');
+				}}
+				className="button"
+				disabled={!text}
+			>
+				<MdSend style={{ height: 15, width: 15 }} />
+			</button>
+		</div>
+	);
+}
+
+function Message({ m, name }) {
+	return (
+		<div className="message-wrap" from={m.name === name ? 'me' : 'you'}>
+			<div className="message">
+				<div className="msg-name">{m.name}</div>
+				<div className="msg-text">
+					{m.text}
+					{m.img && <img src={bucket + m.img + suffix} alt="pic" />}
+				</div>
+			</div>
+		</div>
 	);
 }
 
